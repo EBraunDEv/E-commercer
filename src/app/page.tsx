ad655c3377e8228e335696ac5dff4e8eec26513d
Elmo Braun <elmo.braundev@gmail.com>
2023-11-20 11:15:45 -0300

    Inicio da listagem de produtos parte 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-      <h1>Next E-commecer</h1>
-    </main>
+    <div className="max-w-7xl max-auto py-8 px-8 xl:px-0" >
+      <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6
+       ">
+
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          Poduto
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          Poduto 2
+          </div>
+          <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          Poduto
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          Poduto 2
+          </div>
+
+      </div>
+      
+    </div>
   )
 }
