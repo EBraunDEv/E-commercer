61b53aaf29ba7beed30284a1c4991ac1a922d70c
Elmo Braun <elmo.braundev@gmail.com>
2023-11-21 08:27:37 -0300

    Adicionadno autenticação com Clerk

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { SignedIn, SignedOut } from '@clerk/nextjs/app-beta/client'
+import { SignInButton, UserButton } from "@clerk/nextjs"; 
 
 function Navbar() {
   return (
@@ -6,6 +8,20 @@ function Navbar() {
           <Link className='uppercas font-bold text-md h-12 flex items-center' href={''}>
             Braun Store
           </Link>
+
+          <div className="felx items-center gap-8">
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            <SignedOut>
+             <SignInButton mode='modal' >
+             <button className="border rounded-md border-gray-400 px-3 py-2">
+                  Fazer login
+              </button>
+             </SignInButton>
+            </SignedOut>
+
+          </div>
 
         </nav>
   )
